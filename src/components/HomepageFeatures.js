79eccf0d6f0ff1fc11ab0cdd73c1b872c8ffa2be
Lukas Beranek <beranek.lukas@gmail.com>
2021-11-08 10:55:26 +0100

    init

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,12 +4,11 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Quick setup',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Just connect your organization and start serving modules in no time.
       </>
     ),
     code: (
@@ -19,22 +18,20 @@ const FeatureList = [
     )
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Terraform module registry',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Share your quality modules securely. Know what you're deploying and react.
       </>
     )
   },
   {
-    title: 'Powered by React',
+    title: 'Remote state backend',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Secure remote Terraform state backend regardless of cloud provider. Sync to any backend. Seriously.
       </>
     )
   },
