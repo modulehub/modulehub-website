6407c9cef94514d7077ad9513ba0f026392e3369
Lukas Beranek <beranek.lukas@gmail.com>
2021-11-16 22:17:35 +0100

    wording

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'ModuleHub',
-  tagline: 'Turn your git repositories into modules',
+  tagline: 'Turn your Github repositories into Terraform modules. Get Terraform remote state without any hassle.',
   url: 'https://www.modulehub.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
