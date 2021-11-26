8d0576823db5edb7ec55ebcfe074959488f9e7e3
Lukas Beranek <beranek.lukas@gmail.com>
2021-11-26 22:11:14 +0100

    add discord

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,23 +97,15 @@ const config = {
               },
             ],
           },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/Ra9d3ZT4ed',
+              }
+            ],
+          },
           // {
           //   title: 'More',
           //   items: [
